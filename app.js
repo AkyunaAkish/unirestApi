9203dc4c49677bdc381a3b735f588914375dbf4c
AkyunaAkish <akyunaakish@gmail.com>
2015-07-21 09:50:27 -0600

    fixed NYT API KEY and added res.end to app.get books

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/books', function(req, res) {
-  unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + NYT_API_KEY)
+  unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
         console.log(response.body);
+          res.end('Done')
       })
+
 })
 
 app.use('/', routes);
