b7b2ba955df71403596cbc31c128548bd05485f0
AkyunaAkish <akyunaakish@gmail.com>
2015-07-21 10:34:22 -0600

    retrieved data from nyt api

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/books', function(req, res) {
   unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + process.env.NYT_API_KEY)
       .end(function (response) {
-        console.log(response.body);
-          res.end('Done')
+        console.log(response.body.results.books);
+          var NYTBooks = response.body.results.books;
+          res.render('index', {books: NYTBooks});
+          //res.end('Done')
       })
 
 })
